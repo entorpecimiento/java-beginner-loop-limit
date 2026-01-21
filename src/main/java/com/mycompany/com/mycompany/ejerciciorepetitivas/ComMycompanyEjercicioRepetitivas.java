066db5a562c03f6ab0d4ba6f9c543f5192417a27
entorpecimiento <>
2026-01-20 21:25:13 -0300

    implement dynamic counter with while loop and user input

---
+++ src/main/java/com/mycompany/com/mycompany/ejerciciorepetitivas/ComMycompanyEjercicioRepetitivas.java
@@ -0,0 +1,23 @@
+package com.mycompany.com.mycompany.ejerciciorepetitivas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author entorpecimiento
+ */
+public class ComMycompanyEjercicioRepetitivas {
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese el límite hasta el que quiere contar:");
+        Scanner teclado = new Scanner(System.in);
+        int limite = teclado.nextInt();
+        int cont = 1;
+
+        while (cont <= limite) {
+            System.out.println (cont);
+            cont = cont + 1;
+        }
+
+    }
+}
